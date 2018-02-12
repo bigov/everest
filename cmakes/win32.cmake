9e4f68f6336ce99e490c99c02fd26915114ad041
Igor.Balezin <balezin@yandex.ru>
2018-02-12 20:22:28 +0900

    ch lib list

--- cmakes/win32.cmake
+++ cmakes/win32.cmake
@@ -2,20 +2,12 @@
 # x86_64 - Msys2 - Mingw64
 #
 
-SET( LIBS
-  wx_mswu_richtext-3.1
-  wx_mswu_adv-3.1
-  wx_mswu_html-3.1
-  wx_mswu_core-3.1
-  wx_baseu_xml-3.1
-  wx_baseu-3.1
-  wxpng-3.1
-  wxregexu-3.1
-)
-list( APPEND LIBS expat jpeg tiff lzma z rpcrt4 oleaut32 ole32 uuid uxtheme
-  winspool winmm shell32 shlwapi comctl32 comdlg32 advapi32 version wsock32
-  gdi32 oleacc
-)
+SET( LIBS wx_mswu_richtext-3.1 wx_mswu_adv-3.1 wx_mswu_html-3.1
+ wx_mswu_core-3.1 wx_baseu_xml-3.1 wx_baseu-3.1 wxtiff-3.1 wxjpeg-3.1 wxpng-3.1
+ wxzlib-3.1 wxregexu-3.1 wxexpat-3.1 rpcrt4 oleaut32 ole32 uuid uxtheme
+ winspool winmm shell32 shlwapi comctl32 comdlg32 advapi32 version wsock32
+ gdi32 oleacc )
+
 SET( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mwindows -D__WXMSW__" )
 
 include_directories(
